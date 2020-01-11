9f07c63ef4cb8219cf58c67fec7a21233d44b47f
tudorionutelian <you@example.comionuteliantudor@gmail.com>
2020-01-11 14:49:08 +0000

    Canvas edit

---
+++ 1_1150_TUDOR_IonutELian.js
@@ -0,0 +1,28 @@
+/* --- Pas 1: Identificare canvas */
+
+var canvas = document.getElementById('canvas');
+
+/* --- Pas 2: Creaza un obiect editabil */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* --- Find the canvas */
+/* --- Find the canvas */
+
+
+
+
+document.getElementById('range-valoare').addEventListener('input', function(e){
+    document.getElementById('range-value-setata').innerHTML = e.target.value;
+})
